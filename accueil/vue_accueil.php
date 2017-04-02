1b59a62a052d40a261709ffede7a188c125c0b26
brunoJousse <bruno.jousse1@uqac.ca>
2017-04-03 00:39:36 +0200

    Add files via upload

---
+++ accueil/vue_accueil.php
@@ -0,0 +1,16 @@
+<?php
+	class VueAccueil extends VueGenerique{
+
+		public function affiche(){
+			$this->titre="Accueil";
+			$this->contenu='<br/>
+			<h1> Bienvenue sur TravelExpress</h1>
+				<br/>
+			<p> Ce site vous permet de trouver ou de créer facilement des co-voiturages.</p>	<br/>
+
+<p> Ce site est réalisé par l\'étudiant JOUSSE Bruno dans le cadre du TP3 en Technologies Web Avancées à l\'UQAC. <p>
+<br/>
+			';
+		}
+	}
+?>
